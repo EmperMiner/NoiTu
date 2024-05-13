04c595c9fb3603721636153fa6b1f1d42c0abad1
EmperMiner <6thanhdat@gmail.com>
2024-05-14 00:47:04 +0700

    Optimization from O(n) to O(1)

--- Scripts/test.js
+++ Scripts/test.js
@@ -7,19 +7,11 @@ let word1 = "a", word2, correct, result, score = 0;
 document.getElementById("mySubmit").onclick = function() {
     word2 = document.getElementById("myText").value;
     correct = false;
-    for (var i = 0; i < Object.keys(data[word1]).length; i++) {
-        if (data[word1][i] === word2) { correct = true; }
-    }
-    if (correct){
-        nextWord()
-    }
-    else {
-        document.getElementById("myH2").textContent = 'Từ không tồn tại.';
-    }
+    if (data[word1].includes(word2)) { nextWord(); }
+    else { document.getElementById("myH2").textContent = 'Từ không tồn tại.'; }
 }
 
 function nextWord() {
-    
     document.getElementById("myH2").textContent = `Từ trước là: ${word1} ${word2}`
     word1 = word2;
     document.getElementById("myH1").textContent = `Nối thành công! Từ tiếp theo là: ${word1} _`
