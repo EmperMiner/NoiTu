30812f895869e1c7d22896bc51eecb16b5f594c0
EmperMiner <6thanhdat@gmail.com>
2024-05-14 16:38:45 +0700

    changed word_start.js file to a JSON file

--- Scripts/test.js
+++ Scripts/test.js
@@ -1,39 +1,36 @@
-import data from './test.json' with { type: 'json' };
-import start from './words_start.js';
+import vnDictionary from './test.json' with { type: 'json' };
+import word_start from './words_start.json';
 
-let word1, word2, score = 0;
+let word1 = randomWord(), word2, score;
 gameStart();
-
-//Ấn enter cũng submit
-document.getElementById("myText").addEventListener("keypress", function(event) {
-    // If the user presses the "Enter" key on the keyboard
-    if (event.key === "Enter") { submitWord(); }
-});
-
-document.getElementById("mySubmit").onclick = submitWord();
-function submitWord() {
-    word2 = document.getElementById("myText").value;
-    if (data[word1].includes(word2)) { nextWord(); }
-    else { document.getElementById("myH2").textContent = 'Từ không tồn tại.'; }
-}
 
 function gameStart() {
     let word1 = randomWord();
     document.getElementById("myH1").textContent = `Nối đi! Từ tiếp theo là: ${word1} _`;
+    score = 0;
+    document.getElementById("score").textContent = `Số từ nối được: ${score}`
 }
 
-document.getElementById("reset-btn").onclick = function() {
-    gameStart();
-    score = 0;
-    document.getElementById("score").textContent = `Số từ nối được: ${score}`;
-};
+document.getElementById("reset-btn").onclick = function() { gameStart(); }
+
+//Ấn enter cũng submit
+document.getElementById("myText").addEventListener("keypress", function(event) {
+    if (event.key === "Enter") { submitWord(); }
+});
+document.getElementById("mySubmit").onclick = submitWord();
+
+function submitWord() {
+    word2 = document.getElementById("myText").value;
+    if (vnDictionary[word1].includes(word2)) { nextWord(); }
+    else { document.getElementById("myH2").textContent = 'Từ không tồn tại.'; }
+}
 
 function nextWord() {
     document.getElementById("myText").value = "" //Empty the input text box
     document.getElementById("myH2").textContent = `Từ trước là: ${word1} ${word2}`
     score++;
     document.getElementById("score").textContent = `Số từ nối được: ${score}`
-    if (!data.hasOwnProperty(word2)) { 
+    if (!vnDictionary.hasOwnProperty(word2)) { 
         gameOver();
         return;
     }
@@ -46,6 +43,6 @@ function gameOver() {
 }
 
 function randomWord() {
-    let currentWord = start[Math.floor(Math.random() * start.length)];
+    let currentWord = word_start[Math.floor(Math.random() * word_start.length)];
     return currentWord;
 }
