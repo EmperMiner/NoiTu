be586180cd53f9850f5f9dd406f6fa879d9e3023
EmperMiner <6thanhdat@gmail.com>
2024-05-14 13:59:59 +0700

    Chức năng 2 & 3

--- Scripts/test.js
+++ Scripts/test.js
@@ -12,9 +12,18 @@ document.getElementById("mySubmit").onclick = function() {
 }
 
 function nextWord() {
+    document.getElementById("myText").value = "" //Empty the input text box
     document.getElementById("myH2").textContent = `Từ trước là: ${word1} ${word2}`
-    word1 = word2;
-    document.getElementById("myH1").textContent = `Nối thành công! Từ tiếp theo là: ${word1} _`
     score++;
     document.getElementById("score").textContent = `Số từ nối được: ${score}`
+    if (!thisSession.hasOwnProperty(word2)) { 
+        gameOver();
+        return;
+    }
+    word1 = word2;
+    document.getElementById("myH1").textContent = `Nối thành công! Từ tiếp theo là: ${word1} _`
+}
+
+function gameOver() {
+    document.getElementById("myH1").textContent = `Trò chơi đã kết thúc tại từ ${word2}!`
 }
