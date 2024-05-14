f2f402d7a13f444cbcdc6d290c2ab9e7731b65b2
EmperMiner <6thanhdat@gmail.com>
2024-05-14 15:58:31 +0700

    Thu's branch

--- Scripts/test.js
+++ Scripts/test.js
@@ -1,13 +1,25 @@
 import data from './test.json' with { type: 'json' };
+import start from './words_start.js';
 
-let word1 = "a", word2, correct, result, score = 0;
+let word1, word2, score = 0;
+gameStart();
 
 document.getElementById("mySubmit").onclick = function() {
     word2 = document.getElementById("myText").value;
-    correct = false;
     if (data[word1].includes(word2)) { nextWord(); }
     else { document.getElementById("myH2").textContent = 'Từ không tồn tại.'; }
 }
+
+function gameStart() {
+    let word1 = randomWord();
+    document.getElementById("myH1").textContent = `Nối đi! Từ tiếp theo là: ${word1} _`;
+}
+
+document.getElementById("reset-btn").onclick = function() {
+    gameStart();
+    score = 0;
+    document.getElementById("score").textContent = `Số từ nối được: ${score}`;
+};
 
 function nextWord() {
     document.getElementById("myText").value = "" //Empty the input text box
@@ -24,4 +36,9 @@ function nextWord() {
 
 function gameOver() {
     document.getElementById("myH1").textContent = `Trò chơi đã kết thúc tại từ ${word2}!`
+}
+
+function randomWord() {
+    let currentWord = start[Math.floor(Math.random() * start.length)];
+    return currentWord;
 }
