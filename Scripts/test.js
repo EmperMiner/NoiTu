b959ca41587735df9c748dabea5d9b0b36d4c9d0
TNAT <tnat5002@gmail.com>
2024-05-14 12:03:37 +0700

    add word-start json file

--- Scripts/test.js
+++ Scripts/test.js
@@ -1,8 +1,10 @@
 import data from './test.json' with { type: 'json' };
+import data from './word-start.json' with { type: 'json' };
 console.log(data);
 console.log(data.a[0]);
 
 let word1 = "a", word2, correct, result, score = 0;
+const resetBtn = document.querySelector(".reset-btn");
 
 document.getElementById("mySubmit").onclick = function() {
     word2 = document.getElementById("myText").value;
@@ -18,3 +20,5 @@ function nextWord() {
     score++;
     document.getElementById("score").textContent = `Số từ nối được: ${score}`
 }
+
+resetBtn.addEventListener("click", randomWord)
