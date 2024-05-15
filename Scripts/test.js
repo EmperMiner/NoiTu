b00bdc5b6ce806ea3a4f2f383bd5869e0dd70636
GabrielTempest <tn2005agf@gmail.com>
2024-05-15 10:16:47 +0700

    Signed-off-by: GabrielTempest <tn2005agf@gmail.com>

--- Scripts/test.js
+++ Scripts/test.js
@@ -22,46 +22,47 @@ document.getElementById("myText").addEventListener("keypress", function(event) {
 });
 document.getElementById("mySubmit").onclick = submitWord();
 function submitWord() {word2 = sort_input(document.getElementById("myText").value);
+console.log(word2)
 if (vnDictionary[word1].includes(word2)) { nextWord(); }
 else { document.getElementById("myH2").textContent = 'Từ không tồn tại.'; }
 }
 
 function nextWord() {
-check_existing_value(word1, word2)
-document.getElementById("myText").value = "" //Empty the input text box
-document.getElementById("myH2").textContent = `Từ trước là: ${word1} ${word2}`
-score++;
-document.getElementById("score").textContent = `Số từ nối được: ${score}`;
-if (!vnDictionary.hasOwnProperty(word2)) { 
-    gameOver();
-    return;
-}
-word1 = word2;
-document.getElementById("myH1").textContent = `Nối thành công! Từ tiếp theo là: ${word1} _`;
+    check_existing_value(word1, word2)
+    document.getElementById("myText").value = "" //Empty the input text box
+    document.getElementById("myH2").textContent = `Từ trước là: ${word1} ${word2}`
+    score++;
+    document.getElementById("score").textContent = `Số từ nối được: ${score}`;
+    if (!vnDictionary.hasOwnProperty(word2)) { 
+        gameOver();
+        return;
+    }
+    word1 = word2;
+    document.getElementById("myH1").textContent = `Nối thành công! Từ tiếp theo là: ${word1} _`;
 }
 function randomWord() {
 let currentWord = word_start[Math.floor(Math.random() * word_start.length)];
 return currentWord;
 }
 function sort_input(word){
-word = word.toLowerCase().trim();
-return word
+    word = word.toLowerCase().trim();
+    return word
 }
 //New function
 function check_existing_value(preword, curword){   
-if (previous_value.isEmpty() == true){
-    previous_value[preword] = [curword];
-}
-else if (preword in previous_value){
-    if (previous_value[preword].includes(curword)){
-        document.getElementById("myH2").textContent = 'Lỗi vì từ đã được input :Đ'
+    if (previous_value.isEmpty() == true){
+        previous_value[preword] = [curword];
+    }
+    else if (preword in previous_value){
+        if (previous_value[preword].includes(curword)){
+            document.getElementById("myH2").textContent = 'Lỗi vì từ đã được input :Đ'
+        }
+        else{
+            previous_value[preword].push(curword);
+        }
     }
     else{
-        previous_value[preword].push(curword);
-    }
-}
-else{
-    previous_value[preword] = [curword];
-}   
+        previous_value[preword] = [curword];
+    }   
 }
 document.getElementById("myH2").textContent = 'Bạn chưa nối từ nào!';
