5d1efb5529e19934126bc6f92ac98df181688c33
EmperMiner <6thanhdat@gmail.com>
2024-05-22 18:21:54 +0700

    fixed CSS

--- Scripts/main.js
+++ Scripts/main.js
@@ -10,7 +10,7 @@ function gameStart() {
     score = 0;
     previousValue = {}
 }
-document.getElementById("reset-btn").onclick = function() { location.reload(); } //Refresh page
+document.getElementById("resetButton").onclick = function() { location.reload(); } //Refresh page
 
 //Countdown timer
 var secondsLeft = 30, secondsDisplayed, minutesDisplayed;
@@ -41,7 +41,7 @@ document.getElementById("myText").addEventListener("keypress", function(event) {
     if (event.key === "Enter" && gameEnded == false) { submitWord(); }
 });
 //Fixes the error where submitWord() is called once at runtime
-document.getElementById("otherDisplay").textContent = 'Bạn chưa nối từ nào!'; 
+document.getElementById("otherDisplay1").textContent = 'chưa nối'; 
 
 function submitWord() {
     word2 = (document.getElementById("myText").value).toLowerCase().trim(); //Filter user input
@@ -52,7 +52,8 @@ function nextWord() {
     listOfWords += word1 + ' ' + word2 + ' - ';
     modifyTimer(5) //Add 5 seconds every time you get a word right
     document.getElementById("myText").value = "" //Empty the input text box
-    document.getElementById("otherDisplay").textContent = `Từ trước là: ${word1} ${word2}`
+    document.getElementById("otherDisplay2").textContent = document.getElementById("otherDisplay1").textContent
+    document.getElementById("otherDisplay1").textContent = `${word1} ${word2}`
     score++;
     document.getElementById("scoreDisplay").textContent = `${score}`;
 
@@ -71,7 +72,7 @@ function nextWord() {
 
 function gameLogic() {  
     if(!vnDictionary[word1].includes(word2)) {   
-        document.getElementById("otherDisplay").textContent = 'Từ không tồn tại XD'; 
+        document.getElementById("otherDisplay1").textContent = 'Từ không tồn tại XD'; 
         return;
     }
     
@@ -79,7 +80,7 @@ function gameLogic() {
     if (word1 in previousValue) {
         //Check if word2 is a value of the key word1 in previousValue
         if (previousValue[word1].includes(word2)) {
-            document.getElementById("otherDisplay").textContent = 'Từ đã được sử dụng :P'
+            document.getElementById("otherDisplay1").textContent = 'Từ đã được sử dụng :P'
         }
         else {                
             previousValue[word1].push(word2); //Add value word2 to the key word1
@@ -114,8 +115,8 @@ function gameOver(ending) {
     }
     document.getElementById("myText").style.display = "none";
     document.getElementById("mySubmit").style.display = "none";
-    if (listOfWords === "") { document.getElementById("otherDisplay").textContent = "Sao không nối :("}
-    else { document.getElementById("otherDisplay").textContent = listOfWords; }
+    if (listOfWords === "") { document.getElementById("otherDisplay1").textContent = "Sao không nối :("}
+    else { document.getElementById("otherDisplay1").textContent = listOfWords; }
     
     document.getElementById("scoreDisplay").textContent = `Tổng số từ nối được: ${score}`;
 }
